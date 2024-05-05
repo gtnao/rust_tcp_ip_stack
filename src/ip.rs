be51ad28b13e28e8dfa82f37efc101c7140c25a9
naotaka nakane <n.nakane0219@gmail.com>
2024-05-05 09:30:06 +0000

    Add ip parse.

--- src/ip.rs
+++ src/ip.rs
@@ -1,1 +1,113 @@
+use anyhow::Result;
+
 pub const IP_ADDRESS_LENGTH: u8 = 4;
+
+#[derive(Debug)]
+enum IPVersion {
+    IPv4,
+    IPv6,
+}
+#[derive(Debug)]
+enum IPProtocol {
+    ICMP,
+    TCP,
+    UDP,
+}
+
+#[derive(Debug)]
+pub struct IPPacket {
+    header: IPHeader,
+    data: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub struct IPHeader {
+    version: IPVersion,
+    ihl: u8,
+    precedence: u8,
+    delay: bool,
+    throughput: bool,
+    reliability: bool,
+    total_length: u16,
+    identification: u16,
+    df: bool,
+    mf: bool,
+    fragment_offset: u16,
+    ttl: u8,
+    protocol: IPProtocol,
+    header_checksum: u16,
+    source_ip_address: u32,
+    destination_ip_address: u32,
+    options: Vec<u8>,
+}
+
+impl IPHeader {
+    fn parse(data: &[u8]) -> Result<Self> {
+        let version = match data[0] >> 4 {
+            4 => IPVersion::IPv4,
+            6 => IPVersion::IPv6,
+            _ => return Err(anyhow::anyhow!("Invalid IP version")),
+        };
+        let ihl = data[0] & 0x0F;
+        let precedence = data[1] >> 5;
+        let delay = (data[1] >> 4) & 1 == 1;
+        let throughput = (data[1] >> 3) & 1 == 1;
+        let reliability = (data[1] >> 2) & 1 == 1;
+        let total_length = u16::from_be_bytes([data[2], data[3]]);
+        let identification = u16::from_be_bytes([data[4], data[5]]);
+        let df = (data[6] >> 6) & 1 == 1;
+        let mf = (data[6] >> 5) & 1 == 1;
+        let fragment_offset = u16::from_be_bytes([data[6] & 0x1F, data[7]]);
+        let ttl = data[8];
+        let protocol = match data[9] {
+            1 => IPProtocol::ICMP,
+            6 => IPProtocol::TCP,
+            17 => IPProtocol::UDP,
+            _ => return Err(anyhow::anyhow!("Invalid IP protocol")),
+        };
+        let header_checksum = u16::from_be_bytes([data[10], data[11]]);
+        let source_ip_address = u32::from_be_bytes([data[12], data[13], data[14], data[15]]);
+        let destination_ip_address = u32::from_be_bytes([data[16], data[17], data[18], data[19]]);
+        let options = data[20..(ihl as usize * 4)].to_vec();
+        Ok(IPHeader {
+            version,
+            ihl,
+            precedence,
+            delay,
+            throughput,
+            reliability,
+            total_length,
+            identification,
+            df,
+            mf,
+            fragment_offset,
+            ttl,
+            protocol,
+            header_checksum,
+            source_ip_address,
+            destination_ip_address,
+            options,
+        })
+    }
+    fn data_offset(&self) -> usize {
+        (self.ihl << 2) as usize
+    }
+}
+
+impl IPPacket {
+    pub fn parse(data: Vec<u8>) -> Result<Self> {
+        let header = IPHeader::parse(&data)?;
+        let data = data[header.data_offset()..].to_vec();
+        Ok(IPPacket { header, data })
+    }
+}
+
+// pub struct IPController {}
+//
+// impl IPController {
+//     pub fn new() -> Self {
+//         IPController {}
+//     }
+//
+//     pub fn input(&self, data: Vec<u8>) {}
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::{process, thread, time::Duration};
 
 use anyhow::Result;
-use rust_tcp_ip_stack::net::{LoopbackNetDevice, NetDeviceContext, NetDeviceType, NET_PROTOCOL_IP};
+use rust_tcp_ip_stack::{
+    ip::IPPacket,
+    net::{LoopbackNetDevice, NetDeviceContext, NetDeviceType, NET_PROTOCOL_IP},
+};
 use signal_hook::{consts::TERM_SIGNALS, iterator::Signals};
 
 fn main() -> Result<()> {
@@ -25,6 +28,29 @@ fn main() -> Result<()> {
             process::exit(0);
         });
     });
+
+    // test
+    let packet: Vec<u8> = vec![
+        0x45, 0x00, 0x00, 0x14, 0x00, 0x01, 0x40, 0x00, 0x40, 0x06, 0x00, 0x00, 0xc0, 0xa8, 0x01,
+        0x01, 0xc0, 0xa8, 0x01, 0x02,
+    ];
+    println!("{:?}", IPPacket::parse(packet)?);
+    let packet: Vec<u8> = vec![
+        0x46,       // バージョン4, ヘッダ長6
+        0b10111000, // ToS: 優先度5, D=1, T=1, R=1
+        0x00, 0x37, // 全長
+        0x00, 0x01, // 識別子
+        0b00100000, 0x64, // フラグ: MF, フラグメントオフセット100
+        0x40, // TTL
+        0x06, // プロトコル (TCP)
+        0x00, 0x00, // チェックサム (再計算が必要)
+        0xc0, 0xa8, 0x01, 0x01, // 送信元IPアドレス
+        0xc0, 0xa8, 0x01, 0x02, // 宛先IPアドレス
+        0x01, 0x02, 0x03, 0x04, // オプション (例としてノップ)
+        0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64,
+        0x21, // データ
+    ];
+    println!("{:?}", IPPacket::parse(packet)?);
 
     thread::sleep(Duration::from_secs(1));
     loop {
